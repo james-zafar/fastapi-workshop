0ee291b9f28c7669fec35a54149d20bd3a56547f
jameszafar <jzafar81@gmail.com>
2023-05-29 18:55:15 +0100

    Return correct error response for GET /models/{model_id}

--- app/api/operations/models.py
+++ app/api/operations/models.py
@@ -14,7 +14,7 @@ def get_model(model_id: uuid.UUID, request: Request) -> JSONResponse:
     if not (model := request.app.state.model_store.get(str(model_id))):
         return JSONResponse(content=new_error_response([ModelNotFoundError()]), status_code=404)
     if random.randint(0, 100) % 100 == 0:
-        raise HTTPException(detail=new_error_response([InternalServerError()]), status_code=500)
+        return JSONResponse(content=new_error_response([InternalServerError()]), status_code=500)
     return JSONResponse(content=model[0].json())
 
 
